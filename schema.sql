9e223553c79ef9ed3264c98a9e4b4ccf5fc9ec36
Brianna Roper <133825144+ria876@users.noreply.github.com>
2023-11-14 09:41:14 -0500

    Added Schema

---
+++ schema.sql
@@ -0,0 +1,35 @@
+-- Create table for customers
+CREATE TABLE customers (
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
+
+-- Create table for vehicles
+CREATE TABLE vehicles (
+  vehicle_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  license_plate_number VARCHAR(255) NOT NULL UNIQUE,
+  make VARCHAR(255) NOT NULL,
+  model VARCHAR(255) NOT NULL,
+  year INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Create table for appointments
+CREATE TABLE appointments (
+  appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  license_plate_number VARCHAR(255) NOT NULL,
+  time VARCHAR(255) NOT NULL,
+  date DATE NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  FOREIGN KEY (license_plate_number) REFERENCES vehicles(license_plate_number)
+);
+
+-- Create table for admins
+CREATE TABLE admins (
+  admin_id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
