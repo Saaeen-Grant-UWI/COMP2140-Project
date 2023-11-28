d23cfc546c2cd1a71f68a93f78dcdc02f20d7278
Jada Walters <jada.walters@mymona.uwi.edu>
2023-11-28 03:32:24 -0500

    Added CSS for Registration & Login Page

--- pages/register.php
+++ pages/register.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../stylesheet/register.css"> 
     <title>Document</title>
 </head>
 <body>
@@ -12,14 +13,26 @@
     ?>
     
     <section>
-        <div class="container">
+        <div class="register-container">
             <form action="">
-            <input type="text" placeholder = "Enter First Name" name = "f-name" required>
-            <input type="text" placeholder = "Enter Last Name" name = "l-name" required>
-            <input type="email" placeholder = "Enter Email Address" name = "email" required>
-            <input type="password" placeholder="Enter Password" name="password" required>
-            <input type="password" placeholder="Enter Password" name="confirm-password" required>
-            <button type="submit">Register</button>
+                <h1>Register</h1>
+                <!-- <p>Please fill in this form to create an account.</p> -->
+                <label for="f-name"><b>First Name</b></label>
+                <input type="text" placeholder = "Enter First Name" name = "f-name" required>
+                
+                <label for="l-name"><b>Last Name</b></label>
+                <input type="text" placeholder = "Enter Last Name" name = "l-name" required>
+                
+                <label for="email"><b>Email</b></label>
+                <input type="email" placeholder = "Enter Email Address" name = "email" required>
+                
+                <label for="phone"><b>Phone Number</b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+                
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="confirm-password" required>
+                
+                <button type="submit">Register</button>
             </form>
             <a href="login.php">I already have an account</a>
         </div>
