1fe08b13e10b6ffffe1d34bf8efe7ed9e04b9f08
shaecodes <sinclairroshae@yahoo.com>
2023-11-29 08:10:51 -0500

    Updated header.php

--- pages/includes/header.php
+++ pages/includes/header.php
@@ -1,11 +1,6 @@
 <header>
     <div class="container">
     <p><a href="index.php" style="text-decoration: none; color: inherit;">GramacAuto</a></p>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="#">Auto Services</a></li>
-            <li><a href="#">Sample Report</a></li>
-        </ul>
         <div class="call-to-action-container">
             <a href="login.php" class="login-btn">Login</a>
             <a href="register.php" class="register-btn">Register</a>
