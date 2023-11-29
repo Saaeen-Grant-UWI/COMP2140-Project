950027215ba0e7c446f839291988b1074a46eb7a
shaecodes <sinclairroshae@yahoo.com>
2023-11-29 11:38:59 -0500

    Edited page titles

--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../stylesheet/login.css"> 
 
-    <title>Document</title>
+    <title>Log In</title>
 </head>
 <body>
     
